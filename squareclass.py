b507784d61c71554bb50f436450e72e61d3021f4
BlueCl0wn <45670653+BlueCl0wn@users.noreply.github.com>
2019-06-16 10:23:43 +0200

    d

---
+++ squareclass.py
@@ -0,0 +1,19 @@
+import pygame
+from instances import win
+from vars import *
+
+class square():
+    def __init__(self, x, y, color):
+        self.x = (x - 1) * squarewidth + x_border_distance + 40
+        self.y = (y - 1) * squarewidth + y_border_distance + 40
+        self.width = 110
+        self.color = color
+
+    def draw(self):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def rect_green(self):
+        pygame.draw.rect(win, (255, 0, 0), (self.x, self.y, self.width, self.width), 3)
+
+    def rect_red(self):
+        pygame.draw.rect(win, (0, 255,0), (self.x, self.y, self.width, self.width), 5)
